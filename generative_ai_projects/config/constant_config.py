643e8c57a376ccf6392ca9a4c1a418ff64d6adbf
Suraj Vishwakarma <suraj.vishwakarma@yash.com>
2025-11-20 23:17:56 +0530

    feat: aws s3 config and milvus connection

--- generative_ai_projects/config/constant_config.py
+++ generative_ai_projects/config/constant_config.py
@@ -8,10 +8,6 @@ class Config:
 
     API_URL = os.getenv("API_URL", "http://127.0.0.1:8000")
     SQLITE_DB_URL = os.environ.get("SQLITE_DB_URL")
-    COLLECTION_NAME = os.environ.get("COLLECTION_NAME")
-
-    MILVUS_URI = os.environ.get("MILVUS_URI")
-    MILVUS_TOKEN = os.environ.get("MILVUS_TOKEN")
 
     SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     ALGORITHM = "HS256"
@@ -21,3 +17,14 @@ class Config:
     PROJECT_ROOT = pathlib.Path(__file__).parent.parent.parent
     KNOWLEDGE_BASE_DIR = "./data/knowledge_base_files"
     
+    #milvus config
+    MILVUS_DATABASE = os.environ.get("MILVUS_DATABASE", "")
+    MILVUS_HOST = os.environ.get("MILVUS_HOST", "")
+    MILVUS_PORT = os.environ.get("MILVUS_PORT", "")
+    MILVUS_COLLECTION_NAME = os.environ.get("MILVUS_COLLECTION_NAME", "")
+
+    #aws s3 config
+    S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME", "")
+    AWS_REGION = os.environ.get("AWS_REGION", "")
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
--- generative_ai_projects/main.py
+++ generative_ai_projects/main.py
@@ -377,6 +377,38 @@ class ChatResponse(BaseModel):
     token_usage: dict
     cost_info: dict
     
+#NOTE: this api for testing purpose only need login from ui (not needed for agent flow)
+@app.post("/api/login", response_model=LoginResponse)
+async def login(request: LoginRequest, db: Session = Depends(get_db)):
+    user = authenticate_user(db, request.username, request.password)
+
+    if not user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+    access_token = create_access_token(
+        data={"sub": user.username}
+    )
+    data = {
+        "status": 200,
+        "message": "Login successful",
+        "user": {
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "full_name": user.full_name,
+            "role": user.role,
+
+        },
+        "access_token": access_token,
+        "token_type": "bearer",
+    }
+
+    return data
+
 
 @app.post("/api/v1/ticket_agent", response_model=ChatResponse)
 async def chat_with_ticket_agent(
